bafb8d7bcc90c02ed46936d9b61320c496c78987
ProFatXuanAll <ProFatXuanAll@gmail.com>
2023-08-06 22:44:36 +0800

    revise conf.py

--- source/conf.py
+++ source/conf.py
@@ -3,37 +3,111 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = "ProFatXuanAll's blog"
-copyright = '2023, ProFatXuanAll'
+#######################################################################################################################
+# Project information.
+# See https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information for details.
+#######################################################################################################################
 author = 'ProFatXuanAll'
+copyright = '2023, ProFatXuanAll'
+project = "ProFatXuanAll's blog"
 release = ''
+version = ''
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+#######################################################################################################################
+# General configuration.
+# See https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration for details.
+#######################################################################################################################
+add_function_parentheses = False
+add_module_names = True
+default_role = None
+exclude_patterns = [
+  '.venv/',
+  'venv/',
+]
 extensions = [
-  'sphinx.ext.coverage',
-  'sphinx.ext.intersphinx',
+  # Built-in extensions.
+  'sphinx.ext.autosectionlabel',
+  'sphinx.ext.duration',
   'sphinx.ext.extlinks',
+  'sphinx.ext.githubpages',
+  'sphinx.ext.intersphinx',
   'sphinx.ext.mathjax',
-  'sphinx.ext.napoleon',
   'sphinx.ext.todo',
-  'sphinx.ext.viewcode',
+  # 3rd party extensions.
   'sphinx_copybutton',
+  'sphinx_inline_tabs',
   'sphinxcontrib.bibtex',
 ]
-
+keep_warnings = True
+nitpicky = True
+nitpick_ignore = []
+nitpick_ignore_regex = []
+numfig = False
+primary_domain = None
+root_doc = 'index'
+source_encoding = 'utf-8-sig'
+source_suffix = {
+  '.rst': 'restructuredtext',
+}
 templates_path = ['_templates']
-exclude_patterns = ['.venv']
+today_fmt = '%Y-%m-%d'
 
-language = 'zh-TW'
+#######################################################################################################################
+# Options for internationalization.
+# See https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-internationalization for details.
+#######################################################################################################################
+language = 'zh_TW'
 
-# Generating bibtex automatically.
-bibtex_bibfiles = ['refs.bib']
+#######################################################################################################################
+# Options for math.
+# See https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-math for details.
+#######################################################################################################################
+math_eqref_format = 'eq.{number}'
+math_number_all = False
+math_numfig = False
 
+#######################################################################################################################
+# Options for HTML output.
+# See https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output for details.
+#######################################################################################################################
+html_favicon = 'assets/images/favicon.png'
+html_last_updated_fmt = '%Y-%m-%d'
+html_logo = None
+html_math_renderer = 'mathjax'
+html_search_language = 'zh'
+html_static_path = [
+  'assets',
+]
+html_theme = 'furo'
+html_title = "ProFatXuanAll's blog"
+
+#######################################################################################################################
+# Configuration for `sphinx.ext.autosectionlabel`.
+# See https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html for details.
+#######################################################################################################################
+autosectionlabel_prefix_document = True
+
+#######################################################################################################################
+# Configuration for `sphinx.ext.extlinks`.
+# See https://www.sphinx-doc.org/en/master/usage/extensions/extlinks.html for details.
+#######################################################################################################################
+extlinks_detect_hardcoded_links = True
+
+#######################################################################################################################
+# Configuration for `sphinx.ext.intersphinx`.
+# See https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html for details.
+#######################################################################################################################
+intersphinx_mapping = {
+  'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+  'python': ('https://docs.python.org/3', None),
+  'torch': ('https://pytorch.org/docs/master/', None),
+}
+
+#######################################################################################################################
+# Configuration for `sphinx.ext.mathjax`.
+# See https://www.sphinx-doc.org/en/master/usage/extensions/math.html#sphinx-ext-mathjax-render-math-via-javascript
+# for details.
+#######################################################################################################################
 # Customize latex commands.
 mathjax3_config = {
   'tex':
@@ -97,29 +171,44 @@ mathjax3_config = {
         }
     }
 }
+# Don't change this line unless you want to upgrade `mathjax` dependency.
+mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
 
-# Generate automatic links to following projects.
-intersphinx_mapping = {
-  'numpy': ('http://docs.scipy.org/doc/numpy/', None),
-  'python': ('https://docs.python.org/3', None),
-  'torch': ('https://pytorch.org/docs/master/', None),
-}
-
-# Parse NumPy style docstrings but not google style docstrings.
-napoleon_google_docstring = False
-napoleon_numpy_docstring = True
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-add_function_parentheses = False
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
+#######################################################################################################################
+# Configuration for `sphinx.ext.todo`.
+# See https://www.sphinx-doc.org/en/master/usage/extensions/todo.html for details.
+#######################################################################################################################
 todo_include_todos = True
 
-# Add source suffix.
-source_suffix = ['.rst']
+#######################################################################################################################
+# Configuration for `sphinx_copybutton`.
+# See https://sphinx-copybutton.readthedocs.io/en/latest/ for details.
+#######################################################################################################################
+copybutton_exclude = '.linenos, .go, .gp'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+#######################################################################################################################
+# Configuration for `sphinxcontrib.bibtex`.
+# See https://sphinxcontrib-bibtex.readthedocs.io/en/latest/ for details.
+#######################################################################################################################
+bibtex_bibfiles = ['refs.bib']
 
-html_theme = 'furo'
-html_static_path = ['_static']
+#######################################################################################################################
+# Configuration for `furo` theme.
+# See https://pradyunsg.me/furo/ for details.
+#######################################################################################################################
+html_css_files = [
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css',
+]
+html_theme_options = {
+  'footer_icons':
+    [
+      {
+        'name': 'GitHub',
+        'url': 'https://github.com/ProFatXuanAll/ProFatXuanAll.github.io',
+        'html': '',
+        'class': 'fa-brands fa-solid fa-github fa-2x',
+      },
+    ],
+}
